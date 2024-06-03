70b708308fb64a48980ef3394c05989b83dfa733
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-06-03 21:13:41 +0600

    admin od manage user work complate

--- src/Components/Pages/AdminPages/ManageMember.jsx
+++ src/Components/Pages/AdminPages/ManageMember.jsx
@@ -1,23 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import Swal from 'sweetalert2';
 
 const ManageMember = () => {
     const axiosSecure = useAxiosSecure()
-    const [usersData, setUsersData] = useState([])
-    useEffect(() => {
-        axiosSecure.get("/users")
-            .then(res => setUsersData(res.data))
-    }, [])
+    // const [usersData, setUsersData] = useState([])
+    // useEffect(() => {
+    //     axiosSecure.get("/users")
+    //         .then(res => setUsersData(res.data))
+    // }, [axiosSecure])
+    const { data: usersData = [], refetch } = useQuery({
+        queryKey: ['usersData'],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/users")
+            return res.data
+        }
+    })
+
+    const handleRemove = (email) => {
+        axiosSecure.patch(`/demotionUser?email=${email}`)
+            .then(res => {
+                console.log(res.data);
+                refetch()
+                Swal.fire({
+                    position: "top",
+                    icon: "success",
+                    title: "Make User Done",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+            })
+        console.log(email);
+    }
     return (
         <div className=' bg-slate-200 h-screen'>
             <div className='py-10 bg-[#FFF8F5]'>
                 <h2 className='text-3xl font-bold  ml-10'>Manage Member</h2>
             </div>
-            <div className="overflow-x-auto">
-                <table className="table">
+            <div className="overflow-x-auto p-20 drop-shadow-md">
+                <table className="table bg-white">
                     {/* head */}
-                    <thead>
-                        <tr>
+                    <thead className='bg-[#F63E7B] text-xl'>
+                        <tr className=''>
                             <th>Sl</th>
                             <th>Name</th>
                             <th>Email</th>
@@ -31,7 +57,7 @@ const ManageMember = () => {
                                 <th>{idx + 1}</th>
                                 <td>{user.userName}</td>
                                 <td>{user.userEmail}</td>
-                                <button className='btn btn-ghost'>Remove</button>
+                                <button onClick={() => handleRemove(user.userEmail)} className='btn btn-ghost'>Remove</button>
                             </tr>)
                         }
 
